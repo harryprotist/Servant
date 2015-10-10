f1efd971f741a96205577c074c8f9896db51235a
Ben Sharafian <benms64@gmail.com>
2015-10-09 20:33:16 -0700

    adds some skeleton files

--- active/parse.rb
+++ active/parse.rb
@@ -1,12 +1,18 @@
 #!/usr/bin/ruby
 
-puts case ARGV[0]
-when /^uptime\W*$/i
-  `uptime`
-when /^execute (.+?)$/i
-  `#{$1}`
-when /^commit suicide\W*$/i
-  `killall ruby`
-else
-  "" 
+class ActiveParse
+
+  def self.run(input)
+    case input
+    when /^uptime\W*$/i
+      `uptime`
+    when /^execute (.+?)$/i
+      `#{$1}`
+    when /^commit suicide\W*$/i
+      `killall ruby`
+    else
+      "" 
+    end
+  end
+
 end
--- passive/xmpp.rb
+++ passive/xmpp.rb
@@ -2,39 +2,46 @@
 
 require 'xmpp4r'
 require 'xmpp4r/client'
+require 'socket'
 include Jabber
 
-def connect(user, pass)
-  jid = JID.new(user)
-  client = Client.new(jid)
+class AgentXMPP
 
-  client.connect 
-  client.auth(pass)
-  client.send(Presence.new.set_type(:availible))
-  puts "connected as: #{user}"
-  
-  return client
-end
+  def connect(user, pass)
+    jid = JID.new(user)
+    client = Client.new(jid)
 
-def respond(client, msg)
-  puts "processing message"
-  unless msg.body.nil? and msg.type != :error
-    puts "recv: #{msg.body.chomp}"
-    resp = `ruby active/parse.rb "#{msg.body}"`
-    puts "send: #{resp}" 
-    if $?.success?
-      resp = "?" if resp.chomp.length == 0
-      reply = Message.new(msg.from, resp)
-      reply.type = msg.type
-      client.send(reply)
+    client.connect 
+    client.auth(pass)
+    client.send(Presence.new.set_type(:availible))
+    puts "connected as: #{user}"
+    
+    return client
+  end
+
+  def respond(client, msg)
+    puts "processing message"
+    unless msg.body.nil? and msg.type != :error
+      puts "recv: #{msg.body.chomp}"
+      resp = `ruby active/parse.rb "#{msg.body}"`
+      puts "send: #{resp}" 
+      if $?.success?
+        resp = "?" if resp.chomp.length == 0
+        reply = Message.new(msg.from, resp)
+        reply.type = msg.type
+        client.send(reply)
+      end
     end
   end
+
+  def initialize
+
+    auth = IO.readlines('auth/xmpp').map {|x| x.chomp }
+
+    cli = connect(auth[0], auth[1])
+    cli.add_message_callback {|m| respond(cli, m) }
+    puts "registered callback"
+
+  end
+
 end
-
-auth = IO.readlines('auth/xmpp').map {|x| x.chomp }
-
-cli = connect(auth[0], auth[1])
-cli.add_message_callback {|m| respond(cli, m) }
-puts "registered callback"
-
-sleep
